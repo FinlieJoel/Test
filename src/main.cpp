841331094e7f69486e1c106cc6a6e6fd1c462057
Finlie Orton <forton@softwareinstitute.com>
2022-03-24 17:16:57 +0000

    gameplay code

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+int feature1GamePlayCODE()
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
+
 int feature1UI()
 {
     std::cout << "hello world" << std::endl;
