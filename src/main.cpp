a5608bade5f83f2302d3b47bd17475a2a62a0f7f
Finlie Orton <forton@softwareinstitute.com>
2022-03-24 17:11:11 +0000

    new feature from main branch

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
 
+int newFeatureFromMainBranch()
+{
+    std::cout << "hello world" << std::endl;
+    return 0;
+}
+
 int feature1UI()
 {
     std::cout << "hello world" << std::endl;
